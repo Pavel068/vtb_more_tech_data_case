c78204cc37dea28c49189ab1481be6d131fa78a9
Pavel068 <Pavel068>
2022-10-08 22:44:01 +0300

    Suggestions controller

--- routes/api.js
+++ routes/api.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const NewsController = require('../controllers/NewsController')
+const SuggestionsController = require('../controllers/SuggestionsController')
 
-router.get('/', NewsController.index);
+router.get('/news', NewsController.index);
+router.get('/suggestions', SuggestionsController.index);
 
 module.exports = router
